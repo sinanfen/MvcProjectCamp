31c8d519e80217dfa40a9d318a4d2ff71990ae5d
Sinan <sinanfen@hotmail.com>
2021-04-15 23:52:45 +0300

    .
    Asp.net Mvc

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int ContentID { get; set; }
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+
+
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        public int HeadingID { get; set; }
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public ICollection<Content> Contents { get; set; }
+    }
+}
